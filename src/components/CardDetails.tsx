64040e9ebd213c61b18c25671eee7946ce4f7962
TaisYuri <taisyuri@hotmail.com>
2022-08-21 18:00:09 -0300

    updated layout

--- src/components/CardDetails.tsx
+++ src/components/CardDetails.tsx
@@ -1,22 +1,34 @@
-import { HStack, Icon, Text, VStack } from 'native-base';
+import { Box, HStack, Icon, Text, useTheme, VStack } from 'native-base';
 import {IconProps} from 'phosphor-react-native';
 import { ReactNode } from 'react';
 
 type Props ={
     title: string;
-    descriptions: string;
-    footer: string;
+    descriptions?: string;
+    footer?: string;
     icon: React.ElementType<IconProps>;
     children?: ReactNode;
 }
 
-export function CardDetails({descriptions,footer,icon,title,children}:Props){
+export function CardDetails({descriptions,footer,icon: Icon,title,children}:Props){
+    const {colors} = useTheme();
+
     return(
-        <VStack>
-            <HStack>
-                <Text>ICON</Text>
-                <Text textTransform='uppercase'>{title}</Text>
+        <VStack bg="gray.600" p={5} mt={5} rounded='sm'>
+            <HStack alignItems='center' mb={4}>
+                <Icon color={colors.primary[700]}/>
+                <Text textTransform='uppercase' color="gray.300" ml={2} fontSize='sm'>{title}</Text>
             </HStack>
+            {!!descriptions && 
+                <Text color='gray.100' fontSize='md'>{descriptions}</Text>
+            }
+            { children }
+            {!!footer && 
+                <Box borderTopWidth={1} borderTopColor="gray.400" mt={3}>
+                    <Text mt={3} color='gray.300' fontSize='sm'>{footer}</Text>
+                </Box>
+            }
+            
         </VStack>
     )
 }
--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -1,5 +1,5 @@
-import { useRoute } from "@react-navigation/native";
-import { HStack, ScrollView, Text, useTheme, VStack } from "native-base";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { Box, HStack, ScrollView, Text, useTheme, VStack } from "native-base";
 import { Header } from "../components/Header";
 import firestore from "@react-native-firebase/firestore";
 import { useEffect, useState } from "react";
@@ -7,7 +7,16 @@ import { OrderProps } from "../components/Order";
 import { dateFormat } from "../utils/firestoreDateFormat";
 import { OrderFirestoreDTO } from "../DTOs/OrderFirestoreDTO";
 import { Loading } from "../components/Loading";
-import { Hourglass, CircleWavyCheck } from "phosphor-react-native";
+import {
+  Hourglass,
+  CircleWavyCheck,
+  DesktopTower,
+  ClipboardText,
+} from "phosphor-react-native";
+import { CardDetails } from "../components/CardDetails";
+import { Input } from "../components/Input";
+import { Button } from "../components/Button";
+import { Alert } from "react-native";
 
 type RouteParams = {
   orderId: string;
@@ -24,9 +33,36 @@ export function Details() {
   const [solution, setSolution] = useState("");
   const [orders, setOrders] = useState<OrderDetails>({} as OrderDetails);
 
+  const navigation = useNavigation();
   const route = useRoute();
   const { orderId } = route.params as RouteParams;
   const { colors } = useTheme();
+
+  function handleOrderClose() {
+    if (!solution) {
+      return Alert.alert(
+        "Solicitação",
+        "Informe a solução para encerrar a solicitação"
+      );
+    }
+
+    firestore()
+      .collection<OrderFirestoreDTO>("orders")
+      .doc(orderId)
+      .update({
+        status: "closed",
+        solution,
+        closed_at: firestore.FieldValue.serverTimestamp(),
+      })
+      .then(() => {
+        Alert.alert("Solicitação", "Solicitação encerrada");
+        navigation.goBack();
+      })
+      .catch((error) => {
+        console.log(error);
+        Alert.alert("Solicitação", "Não foi possivel encerrar a solicitação");
+      });
+  }
 
   useEffect(() => {
     firestore()
@@ -63,7 +99,10 @@ export function Details() {
 
   return (
     <VStack flex={1} bg="gray.700">
+      <Box px={5} bg='gray.600'>
+
       <Header title="Solicitações" />
+      </Box>
       <HStack bg="gray.500" justifyContent="center" p={4}>
         {orders.status === "closed" ? (
           <CircleWavyCheck size={22} color={colors.green[300]} />
@@ -79,12 +118,41 @@ export function Details() {
           ml={2}
           textTransform="uppercase"
         >
-          { orders.status === "closed" ? 'Finalizado' : 'Em andamento'}
+          {orders.status === "closed" ? "Finalizado" : "Em andamento"}
         </Text>
       </HStack>
       <ScrollView mx={5} showsHorizontalScrollIndicator={false}>
-
+        <CardDetails
+          title="equipamento"
+          descriptions={`Patrimonio ${orders.patrimony}`}
+          icon={DesktopTower}
+        />
+        <CardDetails
+          title="descrição do problema"
+          descriptions={`${orders.descriptions}`}
+          icon={ClipboardText}
+          footer={`Registrado em ${orders.when}`}
+        />
+        <CardDetails
+          title="solução"
+          icon={CircleWavyCheck}
+          footer={orders.closed && `encerrado em ${orders.closed}`}
+          descriptions={orders.solution}
+        >
+          {orders.status === "open" && (
+            <Input
+              placeholder="Descrição da solução"
+              onChangeText={setSolution}
+              multiline
+              h={24}
+              textAlignVertical="top"
+            />
+          )}
+        </CardDetails>
       </ScrollView>
+      {orders.status === "open" && (
+        <Button title="Encerrar solicitação" onPress={handleOrderClose} m={5} />
+      )}
     </VStack>
   );
 }
